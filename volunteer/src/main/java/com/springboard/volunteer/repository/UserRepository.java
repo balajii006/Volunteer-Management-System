98ae89d8c7289d6b3c7ce7c1bd576a84ab77baa0
Govind Prashant Panajkar <govindapanajkar@gmail.com>
2026-02-22 15:38:51 +0530

    Delete volunteer directory

--- volunteer/src/main/java/com/springboard/volunteer/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springboard.volunteer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springboard.volunteer.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
